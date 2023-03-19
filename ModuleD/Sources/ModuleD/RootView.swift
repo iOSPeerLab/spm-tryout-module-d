485f0627f85b5246f9b3b97427c425a0ad95c3fe
Manuel Teixeira <teixeirasmanuel@gmail.com>
2023-03-19 12:04:44 +0000

    init

---
+++ ModuleD/Sources/ModuleD/RootView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Manuel Teixeira on 19/03/2023.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Hello from Module D")
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
